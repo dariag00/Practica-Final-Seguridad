82613621898eb6360f3bc10f56a94f8ba99f5286
Mario García Gago <mgarcg47@estudiantes.unileon.es>
2018-06-07 19:46:12 +0200

    Acabado Hill

--- src/Hill.java
+++ src/Hill.java
@@ -1,17 +1,14 @@
-import java.math.BigInteger;
 import java.util.ArrayList;
 
 
 public class Hill {
 
     int claveCifrado[][];
-    int claveDescifrado[][];
     String alfabeto;
 
 
     public Hill(String alfabeto) {
         this.claveCifrado = claveCifrado;
-        this.claveDescifrado = claveDescifrado;
         this.alfabeto = alfabeto;
     }
 
@@ -21,14 +18,6 @@ public class Hill {
 
     public void setClaveCifrado(int[][] claveCifrado) {
         this.claveCifrado = claveCifrado;
-    }
-
-    public int[][] getClaveDescifrado() {
-        return claveDescifrado;
-    }
-
-    public void setClaveDescifrado(int[][] claveDescifrado) {
-        this.claveDescifrado = claveDescifrado;
     }
 
     public String getAlfabeto() {
@@ -45,38 +34,90 @@ public class Hill {
         int contador1 = 0;
         int [][] cifrar = new int[2][2];
         int[][] resultadoArrays = new int[2][2];
+        char[][] resultALetra = new char[2][2];
         // Separamos el mensaje en 4
         int contador = 0;
+        int determinante = 0;
+        int[][] adjunta = new int[2][2];
+        adjunta[0][0] = this.claveCifrado[1][1];
+        adjunta[1][1] = this.claveCifrado[0][0];
+        adjunta[0][1] = this.claveCifrado[0][1];
+        adjunta[1][0] = this.claveCifrado[1][0];
+        ArrayList<String> palabros = new ArrayList<>();
+        int inverso = 0;
 
-        for(int i = 0; i<mensaje.length(); i++){
+        for(int i = 0; i<mensaje.length(); i++) {
             contador++;
-            if(contador == 2){
-                for(int k = 0; k < 2; k++ ){
-                    for(int j = 0; j < 2; j++){
+            if (contador == 4) {
+                palabros.add(mensaje.substring(i - 4 + 1, i + 1));
+                contador = 0;
+            }
+        }
+        if(contador != 0) {
+            String resto = mensaje.substring(mensaje.length() - contador, mensaje.length());
 
-                        cifrar[i][j] = letraToNum(mensaje.charAt(contador1), this.alfabeto);
+            System.out.println(resto + " Antes");
+
+
+            for (int i = resto.length(); i < 4; i++) {
+
+                resto = resto + this.alfabeto.charAt(0);
+
+            }
+
+            palabros.add(resto);
+
+
+            System.out.println(resto + " Después");
+        }
+
+        for(String palabro : palabros){
+                for (int k = 0; k < 2; k++) {
+                    for (int j = 0; j < 2; j++) {
+
+                        cifrar[k][j] = letraToNum(palabro.charAt(contador1), this.alfabeto);
                         contador1++;
 
                     }
 
-                }
-                contador = 0;
             }
+            contador1 = 0;
 
-            if(cifrado)
+            if(cifrado) {
                 resultadoArrays = multiply(cifrar, this.claveCifrado);
-            else
-                resultadoArrays = multiply(cifrar, this.claveDescifrado);
+                for (int k = 0; k < 2; k++) {
+                    for (int j = 0; j < 2; j++) {
+                        resultadoArrays[k][j] = (resultadoArrays[k][j])%this.alfabeto.length();
+                        resultALetra[k][j] = numToLetra((resultadoArrays[k][j]), this.alfabeto);
+                    }
+                }
+            }else {
+                determinante = determinant(this.claveCifrado, 2);
+
+                inverso = euclidesExtendido(determinante, 27);
+                System.out.println("Determinante " + determinante);
+                System.out.println("Inverso " + inverso);
+
+                resultadoArrays = multiply(cifrar, adjunta);
+
+                for (int k = 0; k < 2; k++) {
+                    for (int j = 0; j < 2; j++) {
+                        resultadoArrays[k][j] = (resultadoArrays[k][j] * inverso) % this.alfabeto.length();
+                        resultALetra[k][j] = numToLetra((resultadoArrays[k][j]), this.alfabeto);
+                    }
+                }
+            }
 
             for(int k = 0; k < 2; k++ ){
                 for(int j = 0; j < 2; j++) {
 
-                    resultado = resultado + resultadoArrays[k][j];
+                    resultado = resultado + resultALetra[k][j];
 
                 }
 
             }
         }
+        System.out.println("Resultado "+resultado);
         return resultado;
     }
     public static int letraToNum(char letra, String alfabeto){
@@ -118,8 +159,75 @@ public class Hill {
          */
         return c;
     }
-   }
 
 
+    public int determinant(int A[][],int N)
+    {
+        int det=0;
+        if(N == 1)
+        {
+            det = A[0][0];
+        }
+        else if (N == 2)
+        {
+            det = A[0][0]*A[1][1] - A[1][0]*A[0][1];
+        }
+        else
+        {
+            det=0;
+            for(int j1=0;j1<N;j1++)
+            {
+                int[][] m = new int[N-1][];
+                for(int k=0;k<(N-1);k++)
+                {
+                    m[k] = new int[N-1];
+                }
+                for(int i=1;i<N;i++)
+                {
+                    int j2=0;
+                    for(int j=0;j<N;j++)
+                    {
+                        if(j == j1)
+                            continue;
+                        m[i-1][j2] = A[i][j];
+                        j2++;
+                    }
+                }
+                det += Math.pow(-1.0,1.0+j1+1.0)* A[0][j1] * determinant(m,N-1);
+            }
+        }
+        return det;
+    }
 
-
+    public int euclidesExtendido(int a, int b)
+    {
+        int[] resp = new int[3];
+        int x=0,y=0,d=0;
+        if(b==0)
+        {
+            resp[0] = a; resp[1] = 1; resp[2] = 0;
+        }
+        else
+        {
+            int x2 = 1, x1 = 0, y2 = 0, y1 = 1;
+            int q = 0, r = 0;
+            while(b>0)
+            {
+                q = (a/b);
+                r = a - q*b;
+                x = x2-q*x1;
+                y = y2 - q*y1;
+                a = b;
+                b = r;
+                x2 = x1;
+                x1 = x;
+                y2 = y1;
+                y1 = y;
+            }
+            resp[0] = a;
+            resp[1] = x2;
+            resp[2] = y2;
+        }
+        return resp[1];
+    }
+}
